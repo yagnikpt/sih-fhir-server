181aad68339a43baa09610e787d8503555833573
Yagnik Patel <coderisaddicted@gmail.com>
2025-09-30 02:13:26 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from app.routers.conceptmap import router as conceptmap_router
 
 _ = load_dotenv()
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(conceptmap_router)
+
 
 @app.get("/")
 def read_root():
